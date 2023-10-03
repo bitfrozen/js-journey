132f1c25b7266557f8eae290bb9fcde086e7b2c8
Antanas Domarkas <antanas.domarkas@curonia.lt>
2023-10-03 22:20:10 +0300

    Variables

---
+++ modern-js/02_variables/script.js
@@ -0,0 +1,21 @@
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(firstName, lastName);
+
+let age = 30;
+console.log(age);
+
+let score;
+console.log(score);
+
+const arr = [1, 2, 3, 4];
+arr.push(5);
+console.log(arr);
+
+const person = {
+  name: "Brad",
+};
+person.name = "John";
+person.email = "john@example.com";
+console.log(person);
