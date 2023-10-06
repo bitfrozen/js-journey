58cc69082d6e36f06a32dc5793f7f1b4f9df6d62
Antanas Domarkas <antanas.domarkas@curonia.lt>
2023-10-06 16:04:48 +0300

    Numbers challenge

---
+++ modern-js/08_challenge/script.js
@@ -0,0 +1,7 @@
+const x = Math.floor(Math.random() * 100) + 1; // non-uniform
+const y = Math.floor(Math.random() * 50) + 1;
+console.log(`${x} + ${y} = ${x + y}`);
+console.log(`${x} - ${y} = ${x - y}`);
+console.log(`${x} * ${y} = ${x * y}`);
+console.log(`${x} / ${y} = ${x / y}`);
+console.log(`${x} % ${y} = ${x % y}`);
