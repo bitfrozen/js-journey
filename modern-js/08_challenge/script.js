72e8026c6705039f7aebbb20bd2dc5dc7959a93d
Antanas Domarkas <antanas.domarkas@curonia.lt>
2023-11-17 12:40:33 +0200

    Removing everything but the main project

--- modern-js/08_challenge/script.js
+++
@@ -1,7 +0,0 @@
-const x = Math.floor(Math.random() * 100) + 1; // non-uniform
-const y = Math.floor(Math.random() * 50) + 1;
-console.log(`${x} + ${y} = ${x + y}`);
-console.log(`${x} - ${y} = ${x - y}`);
-console.log(`${x} * ${y} = ${x * y}`);
-console.log(`${x} / ${y} = ${x / y}`);
-console.log(`${x} % ${y} = ${x % y}`);
