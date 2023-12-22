3133900dd4a3c2208aa9d03d436e1cb32924597c
Antanas Domarkas <antanas.domarkas@curonia.lt>
2023-12-22 14:23:40 +0200

    fetch chain

---
+++ modern-js/48_fetch_more/script.js
@@ -0,0 +1,16 @@
+function createPost({title, body}) {
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      body,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+      'Token': 'abc123'
+    }
+  }).then(res => res.json())
+    .then(data => console.log(data));
+}
+
+createPost({title: 'My post', body: 'This is my post'});
